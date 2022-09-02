2b5dc09c14d47d927cbffd5c48ebe77358acbc90
QuantoPi <63212590+QuantoPi@users.noreply.github.com>
2022-09-02 15:05:17 -0500

    Update app.js
    added semicolons

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,50 @@
 // modal functions
-const btnCloseModal = document.querySelector('#btn-close-modal')
-const modal = document.querySelector('#modal')
-const input = document.getElementById('name-input')
-modal.showModal()
+const btnCloseModal = document.querySelector('#btn-close-modal');
+const modal = document.querySelector('#modal');
+const input = document.getElementById('name-input');
+modal.showModal();
 var userName;
 
 const isInputEmpty = ()=>{
-    return input.value === "" ? true : false
+    return input.value === "" ? true : false;
 }
 const getName = ()=>{
-    userName = input.value
-    console.log(userName)
-    return userName
+    userName = input.value;
+    console.log(userName);
+    return userName;
 }
 btnCloseModal.addEventListener('click', ()=>{
     if(isInputEmpty()){
-        alert("Username input empty")
+        alert("Username input empty");
     }
     else{
-        getName()
-        modal.close()
+        getName();
+        modal.close();
     }
 })
 // websocket functions
-let url = `ws://${window.location.host}/ws/socket-server`
-const chatSocket = new WebSocket(url)
+let url = `ws://${window.location.host}/ws/socket-server`;
+const chatSocket = new WebSocket(url);
 chatSocket.onmessage = function(e){
-    let = data = JSON.parse(e.data)
-    console.log('Data:', data)
+    let = data = JSON.parse(e.data);
+    console.log('Data:', data);
 
     if(data.type === 'chat'){
-        let messages = document.getElementById('messages')
+        let messages = document.getElementById('messages');
         messages.insertAdjacentHTML('beforeend', `<div class="message">
                 <strong>${data.username}</strong>
                 <p>${data.message}</p>
-            </div>`)
-        console.log(data.username)
+            </div>`);
+        console.log(data.username);
     }
 }
-let form = document.getElementById('form')
+let form = document.getElementById('form');
 form.addEventListener('submit', (e)=> {
-    e.preventDefault()
-    let message = e.target.msg.value
+    e.preventDefault();
+    let message = e.target.msg.value;
     chatSocket.send(JSON.stringify({
         'username':userName,
         'message':message
-    }))
+    }));
     form.reset()
 })
